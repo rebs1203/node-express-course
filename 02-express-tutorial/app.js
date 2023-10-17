5384eb1bd65def44b10b191b747e18f1b891bf66
rebecavg <rebecavgallo@hotmail.com>
2023-10-17 13:15:21 -0400

    finished assignment

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,41 @@
-console.log('Express Tutorial')
+const express = require('express')
+const { products } = require("./data");
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.static("./public"))
+
+app.get('/api/v1/test', (req, res) => {
+    res.json({ message: 'It worked!' })
+})
+
+app.get('/api/v1/products', (req, res) => {
+    res.json(products)
+})
+
+app.get('/api/v1/products/:productID', (req, res) => {
+    const id = parseInt(req.params.productID)
+    const findId = products.find((item) => item.id === id)
+    if (!findId) {
+        res.status(404).send('Product not found')
+    } else {
+        res.json(findId)
+    }
+})
+
+app.get('/api/v1/query', (req, res) => {
+    const search = req.query.search
+    const cost = req.query.cost
+    const filteredProducts = products.filter((items) => items.search === search && items.cost === limit)
+    res.json(filteredProducts)
+})
+
+app.all('*', (req, res) => {
+    res.status(404).send('404 - Not Found');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
