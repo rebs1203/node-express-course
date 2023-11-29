58f7c7153494af13b9bc2f70d4793b0cc7259295
rebecavg <rebecavgallo@hotmail.com>
2023-11-29 14:07:46 -0500

    finished exercise

--- 05-JWT-Basics/starter/app.js
+++ 05-JWT-Basics/starter/app.js
@@ -4,6 +4,7 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+const mainRouter = require('./routes/main')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
---
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -0,0 +1,33 @@
+const {BadRequest} = require('../errors/index')
+const jwt = require('jsonwebtoken')
+
+const login = async (req, res) => {
+    const {username, password} = req.body
+
+    if(!username || !password) {
+        throw new BadRequest('Please provide e-mail and password')
+    }
+
+    const id = new Date().getDate()
+
+    const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn: '30d'})
+
+    res.status(200).json({msg: 'user created', token})
+
+    res.send('Fake Login')
+}
+
+const dashboard = async (req, res) => {
+
+
+    const luckyNumber = Math.floor( Math.random() * 100 )
+
+    res.status(200).json({msg: `Hello, ${req.user.username}`,
+    secret: `Here's your authorized data, your lucky number is ${luckyNumber}`})
+
+}
+
+module.exports = {
+    login,
+    dashboard
+}
