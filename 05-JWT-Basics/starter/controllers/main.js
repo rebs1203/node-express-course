d3c9bfd03da1661f1e4283989d5606de0b364c8f
rebecavg <rebecavgallo@hotmail.com>
2023-12-06 17:35:49 -0500

    fixed errors

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,21 +1,4 @@
 const {BadRequest} = require('../errors/index')
-const jwt = require('jsonwebtoken')
-
-const login = async (req, res) => {
-    const {username, password} = req.body
-
-    if(!username || !password) {
-        throw new BadRequest('Please provide e-mail and password')
-    }
-
-    const id = new Date().getDate()
-
-    const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn: '30d'})
-
-    res.status(200).json({msg: 'user created', token})
-
-    res.send('Fake Login')
-}
 
 const dashboard = async (req, res) => {
 
